5401a4bfb22c448226cf0f6e3dbcaf569982fbc1
Alejo Boga <alejoboga20@gmail.com>
2023-08-01 16:26:54 -0400

    lazy loading login page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,15 +1,25 @@
+/* eslint-disable react-refresh/only-export-components */
+import * as React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-enum Routes {
-	Home = '/',
+const LoginPage = React.lazy(
+	() => /* webpackChunkName: 'LoginPage' */ import('../auth/pages/Login')
+);
+
+export enum Routes {
+	Root = '/',
 	Heroes = '/heroes',
 	NotFound = '*',
 }
 
 export const router = createBrowserRouter([
 	{
-		path: Routes.Home,
-		element: <div>Home Route</div>,
+		path: Routes.Root,
+		element: (
+			<React.Suspense fallback={<div>Loading...</div>}>
+				<LoginPage />
+			</React.Suspense>
+		),
 	},
 	{
 		path: Routes.Heroes,
