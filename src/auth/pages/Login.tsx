f2ba126830066fb28a72064555b0bf1265129fe9
Alejo Boga <alejoboga20@gmail.com>
2023-08-01 16:32:58 -0400

    add form

--- src/auth/pages/Login.tsx
+++ src/auth/pages/Login.tsx
@@ -1,5 +1,15 @@
+import styles from './Login.module.css';
+
 const LoginPage = () => {
-	return <div>LoginPage</div>;
+	return (
+		<div className={styles['page']}>
+			<form className={styles['form']}>
+				<input type='text' name='username' id='username' placeholder='Username' />
+				<input type='password' name='password' id='password' placeholder='Password' />
+				<button>Login</button>
+			</form>
+		</div>
+	);
 };
 
 export default LoginPage;
