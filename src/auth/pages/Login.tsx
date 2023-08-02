88f39b29cc3248217655ea7e040d99cf2ff6d499
Alejo Boga <alejoboga20@gmail.com>
2023-08-02 09:29:16 -0400

    style login page

--- src/auth/pages/Login.tsx
+++ src/auth/pages/Login.tsx
@@ -2,13 +2,16 @@ import styles from './Login.module.css';
 
 const LoginPage = () => {
 	return (
-		<div className={styles['page']}>
-			<form className={styles['form']}>
-				<input type='text' name='username' id='username' placeholder='Username' />
-				<input type='password' name='password' id='password' placeholder='Password' />
-				<button>Login</button>
+		<main className={styles.login}>
+			<form className={styles.login__form}>
+				<div className={styles.login__input}>
+					<input type='text' name='username' id='username' placeholder='Username' />
+					<input type='password' name='password' id='password' placeholder='Password' />
+					<p>Welcome to your hero wiki</p>
+				</div>
+				<button className={styles.login__button}>Login</button>
 			</form>
-		</div>
+		</main>
 	);
 };
 
